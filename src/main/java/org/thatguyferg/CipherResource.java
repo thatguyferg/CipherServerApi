aafb15d43e3fcf668017cf157b53224bd7aa0a3c
Ferg <austen.a.ferguson@gmail.com>
2018-01-31 19:43:44 -0500

    Response is now a JSON

--- src/main/java/org/thatguyferg/CipherResource.java
+++ src/main/java/org/thatguyferg/CipherResource.java
@@ -1,12 +1,25 @@
 package org.thatguyferg;
 import static spark.Spark.*;
+import com.google.gson.Gson;
 
 public class CipherResource {
 
+  public static class Encrypted {
+
+    final private String message;
+
+    public Encrypted (final String message) {
+      this.message = message;
+    }
+  }
+
   public static void main(String[] args) {
+    Gson gson = new Gson();
+
     get("/encrypt/:value", (req, res) -> {
+      res.type("application/json");
       CaesarCipher cipher = new CaesarCipher(4);
-      return "Encrypted: " + cipher.encrypt(req.params(":value"));
-    });
+      return new Encrypted(cipher.encrypt(req.params(":value")));
+    },gson::toJson);
   }
 }
